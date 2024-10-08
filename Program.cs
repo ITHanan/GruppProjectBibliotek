373b6e286d3b940556df7b9736ca6c5818cac75f
ITHanan <155847905+ITHanan@users.noreply.github.com>
2024-10-08 11:47:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GruppProjectBibliotek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, to gruop Bibliotic!");
+        }
+    }
+}
