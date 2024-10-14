da25a810f4bf2a74df337729a196f9fb07bb9000
Busher <besher.abodan@icloud.com>
2024-10-14 18:44:50 +0200

    Added Program.Cs
    Added Program.Cs because there was no Main Method.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GruppProjectBibliotek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LibrarySystem library = new LibrarySystem();
+            DisplayUserLibrarySystemInteraction userInterface = new DisplayUserLibrarySystemInteraction(library);
+            userInterface.RunSystem();
+        }
+    }
+}
