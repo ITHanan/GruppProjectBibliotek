dcc2ffcd4a7fdec142a70fd9380a7712ba805d56
Busher <besher.abodan@icloud.com>
2024-10-16 21:37:52 +0200

    Updated

--- LibrarySystem.cs
+++ LibrarySystem.cs
@@ -2,6 +2,52 @@ namespace GruppProjectBibliotek
 {
     public class LibrarySystem
     {
-       //Ditt arbete här 
+      
+        private BookFunctionCore bookCore; 
+        private BookFunctionServices bookService;
+        public List<Book>books = new List<Book>();
+
+        public LibrarySystem()
+        {
+            books= new List<Book>();
+            bookCore = new BookFunctionCore(); 
+            bookService = new BookFunctionServices(); 
+        }
+
+       
+        public void AddBook()
+        {
+            bookCore.AddBook(); 
+        }
+
+        
+        public void RemoveBook()
+        {
+            bookCore.RemoveBook(); 
+        }
+
+        
+        public void DisplayAllBooks()
+        {
+            bookCore.DisplayAllBooks(); 
+        }
+
+       
+        public void SearchBook(string title)
+        {
+           // bookService.SearchBook(title); 
+        }
+
+       
+        public void CheckOutBook(string title)
+        {
+           // bookService.CheckOut(title);
+        }
+
+        
+        public void ReturnBook(string title)
+        {
+            // bookService.ReturnBook(title); 
+        }
     }
 }
